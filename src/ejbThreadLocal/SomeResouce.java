06b4e0ababa9898dbe65fa4a608f31c14d6aa7d8
Eric <eric@eric.com>
2014-11-10 20:25:01 -0500

    Converting @EJB annotation to @Inject

--- src/ejbThreadLocal/SomeResouce.java
+++ src/ejbThreadLocal/SomeResouce.java
@@ -1,6 +1,6 @@
 package ejbThreadLocal;
 
-import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,7 +9,7 @@ import javax.ws.rs.core.Response;
 
 @Path("/myResource")
 public class SomeResouce {
-	@EJB TopLevelEjbWithContext myEjb;
+	@Inject TopLevelEjbWithContext myEjb;
 	
 	@Produces({ "text/plain" })
 	@GET
